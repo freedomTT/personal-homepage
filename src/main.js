4d820e2c7c5c36db52f5e882ccbe1261487425ef
luozhongdao <768851314@qq.com>
2020-08-14 17:39:49 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import _ from 'lodash';
 import $ from 'jquery';
 import Stats from 'stats-js';
 import * as THREE from 'three';
-import {FlyControls} from "three/examples/jsm/controls/FlyControls.js";
+import {MapControls} from "three/examples/jsm/controls/OrbitControls.js";
 import {MTLLoader} from "three/examples/jsm/loaders/MTLLoader.js";
 import {OBJLoader2} from "three/examples/jsm/loaders/OBJLoader2.js";
 import {MtlObjBridge} from "three/examples/jsm/loaders/obj2/bridge/MtlObjBridge.js";
 
+import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass.js';
+import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import {FilmPass} from 'three/examples/jsm/postprocessing/FilmPass.js';
+import {VignetteShader} from 'three/examples/jsm/shaders/VignetteShader.js';
+import {GammaCorrectionShader} from 'three/examples/jsm/shaders/GammaCorrectionShader.js';
 
 const appClass = function (elementToBindTo) {
 	this.clock = new THREE.Clock();
@@ -18,8 +24,8 @@ const appClass = function (elementToBindTo) {
 
 	this.scene = null;
 	this.cameraDefaults = {
-		posCamera: new THREE.Vector3(0.0, 200.0, 0.0),
-		posCameraTarget: new THREE.Vector3(0.0, 0.0, -1000.0),
+		posCamera: new THREE.Vector3(30.0, 100.0, 200.0),
+		posCameraTarget: new THREE.Vector3(30.0, 85.0, 0.0),
 		near: 0.1,
 		far: 10000,
 		fov: 45
@@ -45,29 +51,27 @@ appClass.prototype = {
 		this.scene = new THREE.Scene();
 
 		this.camera = new THREE.PerspectiveCamera(this.cameraDefaults.fov, this.aspectRatio, this.cameraDefaults.near, this.cameraDefaults.far);
-		this.resetCamera();
-		this.controls = new FlyControls(this.camera, this.renderer.domElement);
-		this.controls.movementSpeed = 1000;
-		this.controls.domElement = this.renderer.domElement;
-		this.controls.rollSpeed = Math.PI / 24;
-		this.controls.autoForward = false;
-		this.controls.dragToLook = false;
+		this.controls = new MapControls(this.camera, this.renderer.domElement);
 
-		let ambientLight = new THREE.AmbientLight('#404040');
-		let directionalLight1 = new THREE.DirectionalLight('#626262');
-		directionalLight1.position.set(-100, -50, 100);
-		let pointLight = new THREE.PointLight('#62000c');
-		pointLight.position.set(0, 0, 0);
-		this.scene.add(directionalLight1);
-		this.scene.add(ambientLight);
-		this.scene.add(pointLight);
+		this.controls.enableDamping = true;
+		this.controls.dampingFactor = 0.05;
+		this.controls.screenSpacePanning = false;
+		this.controls.minDistance = 0;
+		this.controls.maxDistance = 200;
+		this.controls.maxPolarAngle = Math.PI / 2;
+		this.controls.target = this.cameraTarget;
+		this.resetCamera();
+
+		let PointLight = new THREE.PointLight('#ffffff');
+		PointLight.position.set(0, 100, 300);
+		this.scene.add(PointLight);
 
 		let helper = new THREE.GridHelper(1200, 60, 0xFF4444, 0x404040);
 		this.scene.add(helper);
 	},
 
 	initContent: function () {
-		let modelName = 'female02';
+		let modelName = 'room';
 		let scope = this;
 		let objLoader2 = new OBJLoader2();
 		let callbackOnLoad = function (object3d) {
@@ -86,10 +90,21 @@ appClass.prototype = {
 		let mtlLoader = new MTLLoader();
 		mtlLoader.load('./static/model/room.mtl', onLoadMtl);
 	},
+	setEffact: function () {
+
+		this.composer = new EffectComposer(this.renderer);
+		this.composer.addPass(new RenderPass(this.scene, this.camera));
+
+		let effectFilmBW = new FilmPass(0.25, 0.0, 2048, true);
+		let gammaCorrection = new ShaderPass(GammaCorrectionShader);
+		let vignetteShader = new ShaderPass(VignetteShader);
+		this.composer.addPass(gammaCorrection);
+		this.composer.addPass(effectFilmBW);
+		this.composer.addPass(vignetteShader);
+
+	},
 
 	resizeDisplayGL: function () {
-		// this.controls.handleResize();
-
 		this.recalcAspectRatio();
 		this.renderer.setSize(this.canvas.offsetWidth, this.canvas.offsetHeight, false);
 
@@ -116,8 +131,9 @@ appClass.prototype = {
 	render: function () {
 		let delta = this.clock.getDelta();
 		if (!this.renderer.autoClear) this.renderer.clear();
-		this.controls.update(delta);
-		this.renderer.render(this.scene, this.camera);
+		// this.controls.update(delta);
+		// this.renderer.render(this.scene, this.camera);
+		this.composer.render();
 	}
 };
 
@@ -138,5 +154,6 @@ console.log('Starting initialisation phase...');
 app.initGL();
 app.resizeDisplayGL();
 app.initContent();
+app.setEffact();
 
 render();
