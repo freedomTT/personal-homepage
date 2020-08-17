973f8bfb78cb1b6ee3181fe2d616fb461a71748d
luozhongdao <768851314@qq.com>
2020-08-17 17:45:56 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import './style/main.less'
 import _ from 'lodash';
 import $ from 'jquery';
 import Stats from 'stats-js';
+
+import {gsap} from "gsap";
+
 import * as THREE from 'three';
 import {MapControls} from "three/examples/jsm/controls/OrbitControls.js";
 import {MTLLoader} from "three/examples/jsm/loaders/MTLLoader.js";
@@ -15,6 +18,9 @@ import {FilmPass} from 'three/examples/jsm/postprocessing/FilmPass.js';
 import {VignetteShader} from 'three/examples/jsm/shaders/VignetteShader.js';
 import {GammaCorrectionShader} from 'three/examples/jsm/shaders/GammaCorrectionShader.js';
 
+
+import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader.js';
+
 const appClass = function (elementToBindTo) {
 	this.clock = new THREE.Clock();
 	this.renderer = null;
@@ -24,8 +30,8 @@ const appClass = function (elementToBindTo) {
 
 	this.scene = null;
 	this.cameraDefaults = {
-		posCamera: new THREE.Vector3(30.0, 100.0, 200.0),
-		posCameraTarget: new THREE.Vector3(30.0, 85.0, 0.0),
+		posCamera: new THREE.Vector3(30.0, 90.0, 200.0),
+		posCameraTarget: new THREE.Vector3(30.0, 85.0, 45.0),
 		near: 0.1,
 		far: 10000,
 		fov: 45
@@ -34,6 +40,7 @@ const appClass = function (elementToBindTo) {
 	this.cameraTarget = this.cameraDefaults.posCameraTarget;
 
 	this.controls = null;
+	this.mixers = [];
 };
 
 appClass.prototype = {
@@ -46,31 +53,59 @@ appClass.prototype = {
 			antialias: true,
 			autoClear: true
 		});
-		this.renderer.setClearColor(0x050505);
+		this.renderer.setClearColor('#000000');
 
 		this.scene = new THREE.Scene();
+		this.scene.fog = new THREE.FogExp2('#2a2a2a', 0.0025);
 
 		this.camera = new THREE.PerspectiveCamera(this.cameraDefaults.fov, this.aspectRatio, this.cameraDefaults.near, this.cameraDefaults.far);
-		this.controls = new MapControls(this.camera, this.renderer.domElement);
-
-		this.controls.enableDamping = true;
-		this.controls.dampingFactor = 0.05;
-		this.controls.screenSpacePanning = false;
-		this.controls.minDistance = 0;
-		this.controls.maxDistance = 200;
-		this.controls.maxPolarAngle = Math.PI / 2;
-		this.controls.target = this.cameraTarget;
 		this.resetCamera();
 
-		let PointLight = new THREE.PointLight('#ffffff');
+		// this.controls = new MapControls(this.camera, this.renderer.domElement);
+		// this.controls.enableDamping = true;
+		// this.controls.dampingFactor = 0.05;
+		// this.controls.screenSpacePanning = false;
+		// this.controls.minDistance = 0;
+		// this.controls.maxDistance = 200;
+		// this.controls.maxPolarAngle = Math.PI / 2;
+		// this.controls.target = this.cameraTarget;
+
+		let PointLight = new THREE.PointLight('#4a4a4a');
 		PointLight.position.set(0, 100, 300);
 		this.scene.add(PointLight);
 
 		let helper = new THREE.GridHelper(1200, 60, 0xFF4444, 0x404040);
 		this.scene.add(helper);
+
+
+		/**/
+		let loader = new FBXLoader();
+		loader.load('./static/model/Sitting.fbx', (object) => {
+
+			object.mixer = new THREE.AnimationMixer(object);
+			this.mixers.push(object.mixer);
+
+			let action = object.mixer.clipAction(object.animations[0]);
+			action.play();
+
+			object.traverse(function (child) {
+
+				if (child.isMesh) {
+
+					child.castShadow = true;
+					child.receiveShadow = true;
+
+				}
+
+			});
+
+			this.scene.add(object);
+
+		});
+		/**/
 	},
 
-	initContent: function () {
+	initContent: function (callback) {
 		let modelName = 'room';
 		let scope = this;
 		let objLoader2 = new OBJLoader2();
@@ -79,6 +114,8 @@ appClass.prototype = {
 			object3d.rotateY(-Math.PI / 2);
 			scope.scene.add(object3d);
 			console.log('Loading complete: ' + modelName);
+
+			callback && callback();
 		};
 
 		let onLoadMtl = function (mtlParseResult) {
@@ -90,17 +127,18 @@ appClass.prototype = {
 		let mtlLoader = new MTLLoader();
 		mtlLoader.load('./static/model/room.mtl', onLoadMtl);
 	},
+
 	setEffact: function () {
 
 		this.composer = new EffectComposer(this.renderer);
 		this.composer.addPass(new RenderPass(this.scene, this.camera));
 
-		let effectFilmBW = new FilmPass(0.25, 0.0, 2048, true);
-		let gammaCorrection = new ShaderPass(GammaCorrectionShader);
-		let vignetteShader = new ShaderPass(VignetteShader);
-		this.composer.addPass(gammaCorrection);
-		this.composer.addPass(effectFilmBW);
-		this.composer.addPass(vignetteShader);
+		// let effectFilmBW = new FilmPass(0.25, 0.0, 2048, true);
+		// let gammaCorrection = new ShaderPass(GammaCorrectionShader);
+		// let vignetteShader = new ShaderPass(VignetteShader);
+		// this.composer.addPass(gammaCorrection);
+		// this.composer.addPass(effectFilmBW);
+		// this.composer.addPass(vignetteShader);
 
 	},
 
@@ -129,11 +167,77 @@ appClass.prototype = {
 	},
 
 	render: function () {
-		let delta = this.clock.getDelta();
 		if (!this.renderer.autoClear) this.renderer.clear();
-		// this.controls.update(delta);
-		// this.renderer.render(this.scene, this.camera);
-		this.composer.render();
+		if (this.camera.position.z < 60) {
+			this.renderer.render(this.sceneOne, this.cameraOne);
+		} else {
+			this.composer.render();
+		}
+
+
+		if (this.mixers.length > 0) {
+
+			for (var i = 0; i < this.mixers.length; i++) {
+
+				this.mixers[i].update(this.clock.getDelta());
+
+			}
+
+		}
+
+	},
+
+	initAnimate: function () {
+		let tl = gsap.timeline({});
+		let step1 = gsap.to(this.camera.position, 5, {z: 45, y: 85});
+		tl.add(step1);
+
+		let upScroll = function () {
+			tl.pause();
+			tl.progress((tl.progress() * 100 + 1) / 100);
+		};
+
+		let downScroll = function () {
+			tl.pause();
+			tl.progress((tl.progress() * 100 - 1) / 100);
+		};
+
+		$(document).bind('mousewheel DOMMouseScroll', function (event) {
+			let wheel = event.originalEvent.wheelDelta;
+			let detail = event.originalEvent.detail;
+			if (event.originalEvent.wheelDelta) { //判断浏览器IE,谷歌滚轮事件
+				if (wheel > 0) {
+					console.log('上滚');
+					upScroll();
+				}
+				if (wheel < 0) {
+					console.log('下滚');
+					downScroll();
+				}
+			} else if (event.originalEvent.detail) {  //Firefox滚轮事件
+				if (detail > 0) {
+					console.log('下滚');
+					downScroll();
+				}
+				if (detail < 0) {
+					console.log('上滚');
+					upScroll();
+				}
+			}
+		});
+
+
+	},
+
+	initSceneOne: function () {
+		this.sceneOne = new THREE.Scene();
+		this.cameraOne = new THREE.PerspectiveCamera(this.cameraDefaults.fov, this.aspectRatio, this.cameraDefaults.near, this.cameraDefaults.far);
+		let PointLight = new THREE.PointLight('#4a4a4a');
+		PointLight.position.set(0, 100, 300);
+		this.sceneOne.add(PointLight);
+
+		let helper = new THREE.GridHelper(1200, 60, 0xFF4444, 0x404040);
+		this.sceneOne.add(helper);
 	}
 };
 
@@ -150,10 +254,15 @@ let render = function () {
 
 window.addEventListener('resize', resizeWindow, false);
 
-console.log('Starting initialisation phase...');
+console.log('Starting initialisation');
 app.initGL();
+app.initSceneOne();
 app.resizeDisplayGL();
-app.initContent();
+app.initContent(() => {
+	setTimeout(() => {
+		app.initAnimate();
+	}, 1000)
+});
 app.setEffact();
 
 render();
